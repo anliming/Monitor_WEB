568b5354460074f6cd0594ab036f6359e87a1a65
anliming <33714451+anliming@users.noreply.github.com>
2021-11-18 17:44:32 +0800

    Update celeryconfig.py

--- task/celeryconfig.py
+++ task/celeryconfig.py
@@ -1,5 +1,5 @@
 #encoding=utf-8
-BROKER_URL = 'redis://localhost:6379/0' # 使用RabbitMQ作为消息代理
+BROKER_URL = 'redis://localhost:6379/0' # 使用redis作为消息代理
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1' # 把任务结果存在了Redis
 CELERY_TASK_SERIALIZER = 'msgpack' # 任务序列化和反序列化使用msgpack方案
 CELERY_RESULT_SERIALIZER = 'json' # 读取任务结果一般性能要求不高，所以使用了可读性更好的JSON
